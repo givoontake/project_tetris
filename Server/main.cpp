0eb5ebbcacdc8ad2f8588a2b5a8632f8b768f0e5
givoontake <givoontake@gmail.com>
2025-04-27 00:27:45 +0900

    서버 시작을 위한 메인 코드 추가

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,4 +1,23 @@
+#include <vector>
+#include <thread>
+#include "IOCPServer.h"
+
+IOCPServer iocp_server;
+
+void worker_thread()
+{
+	iocp_server.ProcessGQCS();
+}
+
 int main()
 {
+	iocp_server.StartServer();
 
+	std::vector <std::thread> worker_threads;
+	int num_threads = std::thread::hardware_concurrency();
+	for (int i = 0; i < num_threads; ++i)
+		worker_threads.emplace_back(worker_thread);
+	for (auto& th : worker_threads)
+		th.join();
 }
+
