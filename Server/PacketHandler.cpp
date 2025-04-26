28427cba3ab1c94a6b9a2feadcfdbb8c45e7fdee
givoontake <givoontake@gmail.com>
2025-04-27 00:23:50 +0900

    Revert "id 컨테이너를 세션에 통합2"
    This reverts commit a01c1bf50bc6423fa603c81ba5c2ab84ead9e2d5.

--- Server/PacketHandler.cpp
+++ Server/PacketHandler.cpp
@@ -14,7 +14,6 @@ void PacketHandler::ProcessPacket(char* packet)
 	case C2S_LOGIN: {
 		// 받아서 따로 서버에서 변경되는 패킷 내용이 없다.
 		for (auto& user : users) {
-			if (!user.GetUse()) continue;
 			S2C_LOGIN_PACKET* p = reinterpret_cast<S2C_LOGIN_PACKET*>(packet);
 			p->type = S2C_LOGIN;
 			user.SendPacket(reinterpret_cast<char*>(p));
@@ -25,7 +24,6 @@ void PacketHandler::ProcessPacket(char* packet)
 	case C2S_MESSAGE:
 		// 받아서 따로 서버에서 변경되는 패킷 내용이 없다.
 		for (auto& user : users) {
-			if (!user.GetUse()) continue;
 			S2C_MESSAGE_PACKET* p = reinterpret_cast<S2C_MESSAGE_PACKET*>(packet);
 			p->type = S2C_MESSAGE;
 			user.SendPacket(reinterpret_cast<char*>(p));
