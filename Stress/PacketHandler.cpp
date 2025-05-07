436ef4d91afba9b896ee593acd5d1aa410aec079
Kang Eun Hyeok <givoontake@gmail.com>
2025-05-07 20:09:34 +0900

    인덱스 벗어나는 문제 수정

--- Stress/PacketHandler.cpp
+++ Stress/PacketHandler.cpp
@@ -12,7 +12,9 @@ void PacketHandler::ProcessPacket(char* packet)
 {
 	std::array<std::unique_ptr<Session>, MAX_USER>& users = manager_interface->GetSessionList();
 	S2C_TEST_PACKET* p = reinterpret_cast<S2C_TEST_PACKET*>(packet);
-	if (!users[p->id]->GetUse() && p->id >= 0 && p->id < MAX_ARRAY_SIZE) return;
+	if (p->id < 0 || p->id >= MAX_ARRAY_SIZE) return;
+	if (!users[p->id]->GetUse()) return;
+
 	switch (packet[2]) {
 	case S2C_TEST: {
 		long long now_time = manager_interface->GetCurrentTimeMS();
