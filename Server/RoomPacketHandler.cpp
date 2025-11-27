dc480830536c0808dcdc83ff7ae461b2acc1cce8
givoontake <givoontake@gmail.com>
2025-11-28 03:31:26 +0900

    핸들링 패킷 추가

--- Server/RoomPacketHandler.cpp
+++ Server/RoomPacketHandler.cpp
@@ -12,7 +12,25 @@ void RoomPacketHandler::HandlePacket(char* packet, int user_index)
 	switch (packet[2]) {
 	case C2S_ADD_USER: {
 		C2S_ADD_USER_PACKET* recv_p = reinterpret_cast<C2S_ADD_USER_PACKET*>(packet);
-		room->AddUser(server->GetSession(recv_p->id));
+		room->AddUser(server->GetSession(user_index));
+		break;
+	}
+
+	case C2S_READY: {
+		C2S_READY_PACKET* recv_p = reinterpret_cast<C2S_READY_PACKET*>(packet);
+		room->ReadyUser(server->GetSession(user_index)->GetId());
+		break;
+	}
+
+	case C2S_KICK: {
+		C2S_KICK_PACKET* recv_p = reinterpret_cast<C2S_KICK_PACKET*>(packet);
+		room->KickUser(server->GetSession(user_index)->GetId(), recv_p->kick_user_id);
+		break;
+	}
+
+	case C2S_START: {
+		C2S_START_PACKET* recv_p = reinterpret_cast<C2S_START_PACKET*>(packet);
+		room->StartGame(server->GetSession(user_index)->GetId());
 		break;
 	}
 	}
