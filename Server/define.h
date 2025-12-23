f347c25b22c8250523aade99d53f5420ac24decc
givoontake <givoontake@gmail.com>
2025-12-24 03:59:02 +0900

    일부 정의값 새 파일로 이동

--- Server/define.h
+++ Server/define.h
@@ -1,29 +1,6 @@
 #pragma once
 #include <atomic>
-constexpr int FPS = 50;
-constexpr int FRAME_TIME = 1000 / FPS;
-constexpr int MAX_TICK_WORKERS = 4;
-
-constexpr int MOVE_DOWN_TIMEOUT_TICK = FPS / 2;
-//constexpr int INPUT_TICK = FPS / 25;
-constexpr int ADD_GARBAGE_LINE_TICK = FPS * 10;
-
-constexpr int CLEAR_LINE_SCORE = 10;
-
-constexpr int BUF_SIZE = 10240;
-constexpr int MAX_MESSAGE_SIZE = 512;
-constexpr int PORT_NUM = 12345;
-
-constexpr int MAX_USER = 10000;
-constexpr int MAX_ROOM = 5000;
-constexpr int MAX_ROOM_NAME = 48; // 16자 * UTF-8 1문자 크기(3)
-constexpr int MAX_ROOM_PASSWORD = 48;
-constexpr int MAX_ARRAY_SIZE = 127;
-constexpr int ID_SIZE = 16;
-
-constexpr int MAX_USER_ID = 48;
-constexpr int MAX_USER_PASSWORD = 48;
-constexpr int MAX_USER_NAME = 48;
+#include "settings.h"
 
 #pragma pack(push, 1)
 
