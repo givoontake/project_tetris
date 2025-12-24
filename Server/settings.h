e4eb46fe41ed933a4aa0305757e060dfd822b992
givoontake <givoontake@gmail.com>
2025-12-24 20:19:20 +0900

    인풋 틱 값 변경

--- Server/settings.h
+++ Server/settings.h
@@ -20,8 +20,8 @@ constexpr int MAX_USER_ID = 48;
 constexpr int MAX_USER_PASSWORD = 48;
 constexpr int MAX_USER_NAME = 48;
 
-constexpr int MOVE_TIMEOUT_TICK = static_cast<int>(FPS*0.15); // 0.1s
-constexpr int ROTATE_TIMEOUT_TICK = static_cast<int>(FPS * 0.15); // 0.1s
-constexpr int DOWN_TIMEOUT_TICK = static_cast<int>(FPS * 0.5); // 0.5s
-constexpr int DROP_TIMEOUT_TICK = static_cast<int>(FPS * 0.15);; // 0.1s
-constexpr int GARBAGE_LINE_TIMEOUT_TICK = static_cast<int>(FPS * 10);; // 10s
+constexpr int MOVE_TIMEOUT_TICK = static_cast<int>(FPS * 0.05); // 좌우, 아래 이동 3가지
+constexpr int ROTATE_TIMEOUT_TICK = static_cast<int>(FPS * 0.1);
+constexpr int DOWN_TIMEOUT_TICK = static_cast<int>(FPS * 0.5); // 자동 아래 이동 시간
+constexpr int DROP_TIMEOUT_TICK = static_cast<int>(FPS * 0.1);
+constexpr int GARBAGE_LINE_TIMEOUT_TICK = static_cast<int>(FPS * 10);
